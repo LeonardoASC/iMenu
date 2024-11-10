7f6ee72350f8df3af0ea3ae7e78dcd2ab21c5b3c
Mateus Dayrell <mateusdayrell7@outlook.com>
2024-11-10 16:47:51 -0300

    add BaseRepository

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\IBaseRepository;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements IBaseRepository
+{
+    protected Model $model;
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function getAll($request = null)
+    {
+        return $this->model
+            ->withTrashed()
+            ->filter($request)
+            ->orderBy('id')
+            ->paginate(15);
+    }
+
+    public function findById($id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function create($data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($data, $model)
+    {
+        $model->update($data);
+
+        return $model;
+    }
+
+    public function destroy($model)
+    {
+        return $model->delete();
+    }
+
+    public function restore($model)
+    {
+        return $model->restore();
+    }
+
+    public function forceDelete($model)
+    {
+        return $model->forceDelete();
+    }
+
+}
