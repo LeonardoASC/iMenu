34916f4cd8fd1abf24d10954e25965c1eb22e71b
leonardo chaves <leonardoaschaves@gmail.com>
2024-11-10 22:26:37 -0300

    add side bar para authenticatedLayout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,10 +1,177 @@
-import { AdminSideBar } from '@/Components/AdminSideBar';
+import React from "react";
+import {
+    Card,
+    Typography,
+    List,
+    ListItem,
+    ListItemPrefix,
+    ListItemSuffix,
+    Chip,
+    Accordion,
+    AccordionHeader,
+    AccordionBody,
+    Alert,
+    Input,
+} from "@material-tailwind/react";
+import {
+    PresentationChartBarIcon,
+    ShoppingBagIcon,
+    UserCircleIcon,
+    Cog6ToothIcon,
+    InboxIcon,
+    PowerIcon,
+} from "@heroicons/react/24/solid";
+import {
+    ChevronRightIcon,
+    ChevronDownIcon,
+    CubeTransparentIcon,
+    MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import NavLink from '@/Components/NavLink';
+import Dropdown from '@/Components/Dropdown';
+import { Link, usePage } from '@inertiajs/react';
 
 export default function AuthenticatedLayout({ header, children }) {
+    const [open, setOpen] = React.useState(0);
+    const user = usePage().props.auth.user;
+
+
+    const handleOpen = (value) => {
+        setOpen(open === value ? 0 : value);
+    };
+
     return (
-        <div className="min-h-screen bg-gray-100 flex">
-            <AdminSideBar />
-            <main className="ml-[2rem] flex-1 ">{children}</main>
+        <div className="h-screen bg-gray-100 flex">
+            <Card className="h-full w-[20rem] max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+                <div className="mb-2 flex items-center gap-4 p-4">
+                    <img src="https://docs.material-tailwind.com/img/logo-ct-dark.png" alt="brand" className="h-8 w-8" />
+                    <Typography variant="h5" color="blue-gray">
+                        <NavLink
+                            href={route('dashboard')}
+                            active={route().current('dashboard')}
+                        >
+                            Dashboard
+                        </NavLink>
+                    </Typography>
+                    
+                </div>
+                <div className="p-2">
+                    <Input icon={<MagnifyingGlassIcon className="h-5 w-5" />} label="Search" />
+                </div>
+                <List>
+                    <Accordion
+                        open={open === 1}
+                        icon={
+                            <ChevronDownIcon
+                                strokeWidth={2.5}
+                                className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
+                            />
+                        }
+                    >
+                        <ListItem className="p-0" selected={open === 1}>
+                            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
+                                <ListItemPrefix>
+                                    <PresentationChartBarIcon className="h-5 w-5" />
+                                </ListItemPrefix>
+                                <Typography color="blue-gray" className="mr-auto font-normal">
+                                    Dashboard
+                                </Typography>
+                            </AccordionHeader>
+                        </ListItem>
+                        <AccordionBody className="py-1">
+                            <List className="p-0">
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                    </ListItemPrefix>
+                                    Analytics
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                    </ListItemPrefix>
+                                    Reporting
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                    </ListItemPrefix>
+                                    Projects
+                                </ListItem>
+                            </List>
+                        </AccordionBody>
+                    </Accordion>
+                    <Accordion
+                        open={open === 2}
+                        icon={
+                            <ChevronDownIcon
+                                strokeWidth={2.5}
+                                className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`}
+                            />
+                        }
+                    >
+                        <ListItem className="p-0" selected={open === 2}>
+                            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
+                                <ListItemPrefix>
+                                    <ShoppingBagIcon className="h-5 w-5" />
+                                </ListItemPrefix>
+                                <Typography color="blue-gray" className="mr-auto font-normal">
+                                    E-Commerce
+                                </Typography>
+                            </AccordionHeader>
+                        </ListItem>
+                        <AccordionBody className="py-1">
+                            <List className="p-0">
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                    </ListItemPrefix>
+                                    Orders
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                                    </ListItemPrefix>
+                                    Products
+                                </ListItem>
+                            </List>
+                        </AccordionBody>
+                    </Accordion>
+                    <hr className="my-2 border-blue-gray-50" />
+                    <ListItem>
+                        <ListItemPrefix>
+                            <InboxIcon className="h-5 w-5" />
+                        </ListItemPrefix>
+                        Inbox
+                        <ListItemSuffix>
+                            <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
+                        </ListItemSuffix>
+                    </ListItem>
+                    <Link href={route('profile.edit')}>
+                        <ListItem className="flex items-center">
+                            <ListItemPrefix>
+                                <UserCircleIcon className="h-5 w-5" />
+                            </ListItemPrefix>
+                            Profile
+                        </ListItem>
+                    </Link>
+                    <ListItem>
+                        <ListItemPrefix>
+                            <Cog6ToothIcon className="h-5 w-5" />
+                        </ListItemPrefix>
+                        Settings
+                    </ListItem>
+                    <Link href={route('logout')} method="post" as="button">
+                        <ListItem>
+                            <ListItemPrefix>
+                                <PowerIcon className="h-5 w-5" />
+                            </ListItemPrefix>
+                            Log Out
+                        </ListItem>
+                    </Link>
+                </List>
+            </Card>
+            <main className="flex-1 px-6 py-10 ">{children}</main>
         </div>
     );
 }
