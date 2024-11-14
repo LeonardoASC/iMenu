882832bd1ab24d66b520894aa7a3dc6463111322
leonardo chaves <leonardoaschaves@gmail.com>
2024-11-14 11:07:54 -0300

    apenas alterando o local da tabela

--- resources/js/Pages/Admin/Category/Index.jsx
+++ resources/js/Pages/Admin/Category/Index.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { usePage } from '@inertiajs/react';
-import { TableCategory } from '@/Components/TableCategory';
+import { TableCategory } from '@/Pages/Admin/Category/Partials/TableCategory';
 
 
 export default function Index() {
